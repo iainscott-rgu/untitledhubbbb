9c2b1e7d19767b3a05e8f91f18708786916d4933
Iain Scott <odyssey3007@hotmail.com>
2016-03-02 01:14:57 +0000

    go! go! go!

--- Home.php
+++ Home.php
@@ -140,7 +140,7 @@ NEWHTML;
 
         <table class="table2">
             <tr><td colspan="2"><p>Message Board - Spotlight - Services</p></td></tr>
-            <tr class="tr2"><td class="t2"><p>img src="assets/bnbthumb.jpg>Spotlight no. 1</p></td></tr>
+            <tr class="tr2"><td class="t2"><p><img src = "assets/bnbthumb.jpg" id="img" ></p></td></tr>
             <tr class="tr2"><td class="t2"><p>row 2</p></td></tr>
             <tr class="tr2"><td class="t2"><p>row 3</p></td></tr>
             <tr class="tr2"><td class="t2"><p>row 4</p></td></tr>
