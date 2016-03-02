0b5270cc71c558cf85f01e479d975af677137942
Iain Scott <odyssey3007@hotmail.com>
2016-03-02 22:54:35 +0000

    go! go! go!

--- Test.php
+++ Test.php
@@ -1,3 +1,57 @@
+<html>
+<section id = "container2">
+eeeeeeeeeee
+    <header>
+<section id = "banner2">
+
+    eeeeeeeeeeee
+
+
+</section>
+    <section id = "navigation2">
+
+        eeeeeeeeeeeeeee
+
+    </section>
+
+
+    </header>
+
+
+    <section id = "content2">
+
+        eeeeeeeeeeeeee
+
+
+    </section>
+
+
+
+
+
+
+
+
+    <section id = "foot2">
+    <footer>
+
+        eeeeeeeeeeeeeeeeee
+
+    </footer>
+
+    </section>
+
+
+
+
+
+</section>
+
+
+
+
+</html>
+
 <?php
 /**
  * Created by PhpStorm.
@@ -5,3 +59,6 @@
  * Date: 02/03/2016
  * Time: 22:44
  */
+
+
+
