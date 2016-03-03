deb6a688424802d4aa947b52856d7454fae104ec
iainscott-rgu <odyssey3007@hotmail.com>
2016-03-03 16:09:16 +0000

    go! go! go!

--- SearchBB.php
+++ SearchBB.php
@@ -35,7 +35,7 @@
     <form action="searchResultsPage.php" method="post">
         <table class="table1">
             <tr><td colspan="2">Search:</td></tr>
-            <tr><td class="small">Let's get Started!</td></tr>
+            <tr><td class="smallfont">Let's get Started!</td></tr>
             <tr><td><label for="location">Pick a location:</label></td><td><select class="inputform" id="location" name="location">
 
                     <?php
