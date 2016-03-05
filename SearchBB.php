be65911a0b886717d6b1c2e76549dbf804f84a5e
Iain Scott <odyssey3007@hotmail.com>
2016-03-05 03:29:50 +0000

    go! go! go!

--- SearchBB.php
+++ SearchBB.php
@@ -148,17 +148,17 @@ NEWHTML;
 <main>
 <section class="container2" id="spotlight">
     <img src="assets/london.jpg" id="img2">
-    LONDON
+    <p>LONDON</p>
 </section>
 
     <section class="container2" id="spotlight2">
         <img src="assets/london.jpg" id="img2">
-        LONDON
+        <p>LONDON</p>
     </section>
 
     <section class="container2" id="spotlight3">
         <img src="assets/london.jpg" id="img2">
-        LONDON
+        <p>LONDON</p>
     </section>
 
 
