120e36f5dfb50ab12a4c34714a3a646fa156b7c3
Iain Scott <odyssey3007@hotmail.com>
2016-03-05 21:01:32 +0000

    go! go! go!

--- Home.php
+++ Home.php
@@ -200,12 +200,10 @@ NEWHTML;
 
         <script language="javascript">
             function toggle() {
-                var ele = document.getElementById("toggleText");
-                if(ele.style.display == "block") {
-                    ele.style.display = "none";
-                }
-                else {
-                    ele.style.display = "block";
+                var rows = document.getElementsByName("toggleText");
+                for(var i=0; i<rows.length; i++)
+                {
+                    rows[i].className = (rows[i].className == "hiddenRow") ? "" : "hiddenRow";
                 }
             }
         </script>
