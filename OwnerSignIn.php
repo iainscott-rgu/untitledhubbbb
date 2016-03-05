5b1898f5240f1603d0487a6bde007270f15bcc1d
Iain Scott <odyssey3007@hotmail.com>
2016-03-05 00:35:59 +0000

    go! go! go!

--- OwnerSignIn.php
+++ OwnerSignIn.php
@@ -115,7 +115,20 @@ session_start();
 
 <section class="container" id="foot">
 
+    <div id="footernav">
+        <nav role="sub">
+            <ul>
+                <li><a href="B&Bregistration.html">Contact</a></li>
+                <li><a href="B&Bregistration.html">Register</a></li>
+                <li><a href="OwnerSignIn.php">Member Area</a></li>
+                <li><a href="SearchBB.php">Search</a></li>
+            </ul>
+        </nav>
+    </div>
+<div id="copyright">
+    <hr width="100%" size="1">
     <p>Copyright. Team D Solutions.</p>
+    </div>
 
 </section>
 
