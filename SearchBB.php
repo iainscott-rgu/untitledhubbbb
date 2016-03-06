c6a72c6c6260bdb60c2e511e55305747bc452f02
Iain Scott <odyssey3007@hotmail.com>
2016-03-06 01:24:43 +0000

    go! go! go!

--- SearchBB.php
+++ SearchBB.php
@@ -234,135 +234,63 @@ NEWHTML;
 
 <div id="East" class="tabcontent">
     <h3>East</h3>
-    <ul><li><a href='#'>Ampthill&Flitwick</a></li>
-        <li><a href='#'>Basildon</a></li>
-        <li><a href='#'>Bedford</a></li>
-        <li><a href='#'>Benfleet</a></li>
-        <li><a href='#'>Berkhamsted</a></li>
-        <li><a href='#'>Biggleswade</a></li>
-        <li><a href='#'>Billericay</a></li>
-        <li><a href='#'>Bishops Stortford</a></li>
-        <li><a href='#'>Braintree</a></li>
-        <li><a href='#'>Breckland</a></li>
-        <li><a href='#'>Brentwood</a></li>
-        <li><a href='#'>Broadland District</a></li>
-        <li><a href='#'>Bury St Edmunds</a></li>
-        <li><a href='#'>Bushey</a></li>
-        <li><a href='#'>Cambridge</a></li>
-        <li><a href='#'>Canvey Island</a></li>
-        <li><a href='#'>Chelmsford</a></li>
-        <li><a href='#'>Cheshunt Waltham Cross</a></li>
-        <li><a href='#'>Chorleywood</a></li>
-        <li><a href='#'>Clacton-on-Sea</a></li>
-        <li><a href='#'>Colchester</a></li>
-        <li><a href='#'>Debenham - Framlingham</a></li>
-        <li><a href='#'>Dengie Peninsula</a></li>
-        <li><a href='#'>Dunstable</a></li>
-        <li><a href='#'>Epping Forest</a></li>
-        <li><a href='#'>Fenland</a></li>
-        <li><a href='#'>Great Dunmow</a></li>
-        <li><a href='#'>Great Yarmouth</a></li>
-        <li><a href='#'>Harlow</a></li>
-        <li><a href='#'>Harpenden</a></li>
-        <li><a href='#'>Hatfield</a></li>
-        <li><a href='#'>Haverhill</a></li>
-        <li><a href='#'>Hemel Hempstead</a></li>
-        <li><a href='#'>Hemsby</a></li>
-        <li><a href='#'>Hertford</a></li>
-        <li><a href='#'>Hunstanton and Heacham</a></li>
-        <li><a href='#'>Huntingdon</a></li>
-        <li><a href='#'>Ipswich</a></li>
-        <li><a href='#'>King's Lynn</a></li>
-        <li><a href='#'>Leigh on sea UK</a></li>
-        <li><a href='#'>Leighton Buzzard</a></li>
-        <li><a href='#'>Letchworth</a></li>
-        <li><a href='#'>Linton UK</a></li>
-        <li><a href='#'>Luton</a></li>
-        <li><a href='#'>Newmarket</a></li>
-        <li><a href='#'>North Norfolk</a></li>
-        <li><a href='#'>Norwich</a></li>
-        <li><a href='#'>Peterborough</a></li>
-        <li><a href='#'>Potters Bar</a></li>
-        <li><a href='#'>Rayleigh</a></li>
-        <li><a href='#'>Rickmansworth</a></li>
-        <li><a href='#'>Royston</a></li>
-        <li><a href='#'>Saffron Walden</a></li>
-        <li><a href='#'>Sandy and Potton UK</a></li>
-        <li><a href='#'>Sawston</a></li>
-        <li><a href='#'>Shefford and Henlow</a></li>
-        <li><a href='#'>South Norfolk</a></li>
-        <li><a href='#'>Southend-on-Sea</a></li>
-        <li><a href='#'>St Albans</a></li>
-        <li><a href='#'>St. Neots</a></li>
-        <li><a href='#'>Stevenage</a></li>
-        <li><a href='#'>Stowmarket</a></li>
-        <li><a href='#'>Sudbury & Hadleigh</a></li>
-        <li><a href='#'>Thurrock(West) - Grays, South Ockendon & Tilbury</a></li>
-        <li><a href='#'>Tring</a></li>
-        <li><a href='#'>Waltham Abbey</a></li>
-        <li><a href='#'>Watford</a></li>
-        <li><a href='#'>Waveney</a></li>
-        <li><a href='#'>Welwyn Garden City</a></li>
-        <li><a href='#'>Westcliff-on-Sea</a></li>
-        <li><a href='#'>Wickford</a></li>
-        <li><a href='#'>Wixams Bedfordshire</a></li>
-    </ul>
+    <p>To search all B&B's in the East Region click here....</p>
 
 </div>
 
 <div id="London" class="tabcontent">
     <h3>London</h3>
-    <p>List...</p>
+    <p>To search all B&B's in the London Region click here....</p>
 </div>
 
 <div id="North%20East" class="tabcontent">
     <h3>North East</h3>
-    <p>List of towns..</p>
+    <p>To search all B&B's in the North East Region click here....</p>
 </div>
 
 <div id="North%20West" class="tabcontent">
     <h3>North West</h3>
-    <p>List of towns..</p>
+    <p>To search all B&B's in the North West Region click here....</p>
 </div>
 
 <div id="Northern%20Ireland" class="tabcontent">
     <h3>Northern Ireland</h3>
-    <p>List of towns..</p>
+    <p>To search all B&B's in the NI Region click here....</p>
 </div>
 
 <div id="Scotland" class="tabcontent">
     <h3>Scotland</h3>
-    <p>List of towns..</p>
+    <p>To search all B&B's in Scotland click here....</p>
 </div>
 
 <div id="South%20East" class="tabcontent">
     <h3>South East</h3>
-    <p>List of towns..</p>
+    <p>To search all B&B's in the South East Region click here....</p>
 </div>
 
 <div id="East%20Midlands" class="tabcontent">
     <h3>East Midlands</h3>
-    <p>List of towns..</p>
+    <p>To search all B&B's in the East Midlands Region click here....</p>
 </div>
 
 <div id="South%20West" class="tabcontent">
     <h3>South West</h3>
-    <p>List of towns..</p>
+    <p>To search all B&B's in the South West Region click here....</p>
 </div>
 
 <div id="Wales" class="tabcontent">
     <h3>Wales</h3>
-    <p>List of towns..</p>
+    <p>To search all B&B's in Wales click here....</p>
 </div>
 
 <div id="West%20Midlands" class="tabcontent">
     <h3>West Midlands</h3>
-    <p>List of towns..</p>
+    <p>To search all B&B's in the West Midlands Region click here....</p>
 </div>
 
 <div id="Yorkshire" class="tabcontent">
     <h3>Yorkshire</h3>
-    <p>List of towns..</p>
+    <p>To search all B&B's in the Yorkshire Region click here....</p>
 </div>
 
     <script>
