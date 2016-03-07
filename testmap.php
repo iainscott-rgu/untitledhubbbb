f1c4f5a306997353fd017bea6a2c09ef8a2e36d4
Iain Scott <odyssey3007@hotmail.com>
2016-03-07 22:37:32 +0000

    go!

--- testmap.php
+++ testmap.php
@@ -1,12 +1,49 @@
 <?php
+
+function parseToXML($htmlStr)
+{
+$xmlStr=str_replace('<','&lt;',$htmlStr);
+$xmlStr=str_replace('>','&gt;',$xmlStr);
+$xmlStr=str_replace('"','&quot;',$xmlStr);
+$xmlStr=str_replace("'",'&#39;',$xmlStr);
+$xmlStr=str_replace("&",'&amp;',$xmlStr);
+return $xmlStr;
+}
+$conn = new PDO ( "sqlsrv:server = tcp:bbsqldb.database.windows.net,1433; Database = SQL_BB", "teamdsqldb", "Sql20022016*");
+$conn->setAttribute( PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION );
+
+
+
+
+// Select all the rows in the markers table
+$query = "SELECT * FROM [B&B] WHERE [city] = 'Aberdeen'";
+$result = sql_query($query);
+if (!$result) {
+    die('Invalid query: ' . sql_error());
+}
+
 header("Content-type: text/xml");
-echo "<?xml version='1.0' encoding='UTF-8'?>";
-echo "<note>";
-echo "<from>Jani</from>";
-echo "<to>Tove</to>";
-echo "<message>Remember me this weekend</message>";
-echo "</note>";
+
+// Start XML file, echo parent node
+echo '<markers>';
+
+// Iterate through the rows, printing XML nodes for each
+while ($row = @sql_fetch_assoc($result)){
+    // ADD TO XML DOCUMENT NODE
+    echo '<marker ';
+    echo 'name="' . parseToXML($row['bbname']) . '" ';
+    echo 'address="' . parseToXML($row['address']) . '" ';
+    echo 'lat="' . $row['latitude'] . '" ';
+    echo 'lng="' . $row['longitude'] . '" ';
+    echo 'type="' . $row['pets'] . '" ';
+    echo '/>';
+}
+
+// End XML file
+echo '</markers>';
+
 ?>
+
 <html>
 <head>
 
