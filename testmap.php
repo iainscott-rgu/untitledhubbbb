8e300653946828ef621602a29151735001e49484
Iain Scott <odyssey3007@hotmail.com>
2016-03-07 22:12:10 +0000

    go!

--- testmap.php
+++ testmap.php
@@ -1,166 +1,17 @@
 <html>
 <head>
 
-
     <?php
-
-    $conn = new PDO ( "sqlsrv:server = tcp:bbsqldb.database.windows.net,1433; Database = SQL_BB", "teamdsqldb", "Sql20022016*");
-    $conn->setAttribute( PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION );
-    try{
-        $st = $conn-> query("SELECT * FROM [B&B] WHERE [city] = 'Aberdeen'");
-
-
-        foreach($st->fetchAll() as $row) {
-            $newhtml =
-                <<<NEWHTML
-                    <div class="">
-    <p>According to our database, your search of: <strong>{$row[city]}</strong> has returned the following results: </p>
-    <p><strong>{$row[city]}</strong></p>
-    <p><strong>{$row[bbname]}</strong></p>
-    <p><strong>{$row[address]}</strong></p>
-    <p><strong>{$row[email]}</strong></p>
-    <p><strong>{$row[latitude]}</strong></p>
-    <p><strong>{$row[longitude]}</strong></p>
-
-
-    <p><a href="Customerinfo.html"><input type="submit" value="BOOK" /></a></p>
-
-
-
-</div>
-NEWHTML;
-            print($newhtml);
-        }
-    }
-    catch(PDOException $e)
-    {print"$e";}
+    header("Content-type: text/xml");
+    echo "<?xml version='1.0' encoding='UTF-8'?>";
+    echo "<note>";
+    echo "<from>Jani</from>";
+    echo "<to>Tove</to>";
+    echo "<message>Remember me this weekend</message>";
+    echo "</note>";
     ?>
-
-
-
-    <?
-    $doc = new_xmldoc('1.0');
-    $root = $doc->add_root('members');
-    $member = $root->new_child('member','');
-
-    $member->new_child('lastName','John');
-    $member->new_child('firstName','Adams');
-    $member->new_child('contribution','3400');
-
-    $member = $root->new_child('member','');
-
-    $member->new_child('lastName','Debra');
-    $member->new_child('firstName','Hones');
-    $member->new_child('contribution','2400');
-
-    $member = $root->new_child('member','');
-
-    $member->new_child('lastName','Jake');
-    $member->new_child('firstName','Tudor');
-    $member->new_child('contribution','1200');
-
-    $fp = @fopen('members.xml','w');
-    if(!$fp) {
-        die('Error cannot create XML file');
-    }
-    fwrite($fp,$doc->dumpmem());
-    fclose($fp);
-    ?>
-
-
 </head>
 
-<body>
-
-here is some text
-
-</body>
-</html>
-
-?>
-<!DOCTYPE html >
-<head>
-    <meta name="viewport" content="initial-scale=1.0, user-scalable=no" />
-    <meta http-equiv="content-type" content="text/html; charset=UTF-8"/>
-    <title>PHP/MySQL & Google Maps Example</title>
-    <script src="https://maps.googleapis.com/maps/api/js?key=YOUR_API_KEY"
-            type="text/javascript"></script>
-    <script type="text/javascript">
-        //<![CDATA[
-
-        var customIcons = {
-            restaurant: {
-                icon: 'http://labs.google.com/ridefinder/images/mm_20_blue.png'
-            },
-            bar: {
-                icon: 'http://labs.google.com/ridefinder/images/mm_20_red.png'
-            }
-        };
-
-        function load() {
-            var map = new google.maps.Map(document.getElementById("map"), {
-                center: new google.maps.LatLng(47.6145, -122.3418),
-                zoom: 13,
-                mapTypeId: 'roadmap'
-            });
-            var infoWindow = new google.maps.InfoWindow;
-
-            // Change this depending on the name of your PHP file
-            downloadUrl("testmap.php", function(data) {
-                var xml = data.responseXML;
-                var markers = xml.documentElement.getElementsByTagName("marker");
-                for (var i = 0; i < markers.length; i++) {
-                    var name = markers[i].getAttribute("name");
-                    var address = markers[i].getAttribute("address");
-                    var type = markers[i].getAttribute("type");
-                    var point = new google.maps.LatLng(
-                        parseFloat(markers[i].getAttribute("lat")),
-                        parseFloat(markers[i].getAttribute("lng")));
-                    var html = "<b>" + name + "</b> <br/>" + address;
-                    var icon = customIcons[type] || {};
-                    var marker = new google.maps.Marker({
-                        map: map,
-                        position: point,
-                        icon: icon.icon
-                    });
-                    bindInfoWindow(marker, map, infoWindow, html);
-                }
-            });
-        }
-
-        function bindInfoWindow(marker, map, infoWindow, html) {
-            google.maps.event.addListener(marker, 'click', function() {
-                infoWindow.setContent(html);
-                infoWindow.open(map, marker);
-            });
-        }
-
-        function downloadUrl(url, callback) {
-            var request = window.ActiveXObject ?
-                new ActiveXObject('Microsoft.XMLHTTP') :
-                new XMLHttpRequest;
-
-            request.onreadystatechange = function() {
-                if (request.readyState == 4) {
-                    request.onreadystatechange = doNothing;
-                    callback(request, request.status);
-                }
-            };
-
-            request.open('GET', url, true);
-            request.send(null);
-        }
-
-        function doNothing() {}
-
-        //]]>
-
-    </script>
-
-</head>
-
-<body onload="load()">
-<div id="map" style="width: 500px; height: 300px"></div>
 </body>
 
 </html>
