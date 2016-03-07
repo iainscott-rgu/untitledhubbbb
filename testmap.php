34366c0fe3a09811381bb4ee62c4aa4f4d4eecf7
Iain Scott <odyssey3007@hotmail.com>
2016-03-07 22:52:52 +0000

    go!

--- testmap.php
+++ testmap.php
@@ -8,7 +8,7 @@
     $conn = new PDO ( "sqlsrv:server = tcp:bbsqldb.database.windows.net,1433; Database = SQL_BB", "teamdsqldb", "Sql20022016*");
     $conn->setAttribute( PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION );
     try{
-        $st = $conn-> query("SELECT * FROM [B&B] WHERE [city] = '$city'");
+        $st = $conn-> query("SELECT * FROM [B&B] WHERE [city] = 'Aberdeen'");
 
 
         foreach($st->fetchAll() as $row) {
