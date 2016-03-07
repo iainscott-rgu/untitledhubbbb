6044c4c87f981bf8e5d192609d13232585fa819c
iainscott-rgu <odyssey3007@hotmail.com>
2016-03-07 10:57:40 +0000

    go!

--- searchResultsPage.php
+++ searchResultsPage.php
@@ -73,7 +73,7 @@
         foreach($st->fetchAll() as $row) {
             $newhtml =
                 <<<NEWHTML
-                    <div class="table3">
+                    <div class="table4">
     <p>According to our database, your search of: <strong>{$row[city]}</strong> has returned the following results: </p>
     <p><strong>{$row[city]}</strong></p>
     <p><strong>{$row[bbname]}</strong></p>
