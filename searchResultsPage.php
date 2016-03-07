a759ee60b2359608da9cd027aeee088ca243f432
Iain Scott <odyssey3007@hotmail.com>
2016-03-07 21:26:41 +0000

    go!

--- searchResultsPage.php
+++ searchResultsPage.php
@@ -89,8 +89,7 @@
     <p><strong>{$row[bbname]}</strong></p>
     <p><strong>{$row[address]}</strong></p>
     <p><strong>{$row[email]}</strong></p>
-    <p><strong>{$row[latitude]}</strong></p>
-    <p><strong>{$row[longitude]}</strong></p>
+
 
 
 
