f4276b1adb32d7c54f19034b34c93e221620290c
Iain Scott <odyssey3007@hotmail.com>
2016-03-07 23:04:51 +0000

    go!

--- testmap.php
+++ testmap.php
@@ -3,47 +3,16 @@
 
 
 
+
     <?php
-    $city = $_POST['location'];
+
     $conn = new PDO ( "sqlsrv:server = tcp:bbsqldb.database.windows.net,1433; Database = SQL_BB", "teamdsqldb", "Sql20022016*");
     $conn->setAttribute( PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION );
     try{
-        $st = $conn-> query("SELECT * FROM [B&B] WHERE [city] = 'Aberdeen'");
+    $st = $conn-> query("SELECT * FROM [B&B] WHERE [city] = 'Aberdeen'");
 
 
-        foreach($st->fetchAll() as $row) {
-            $newhtml =
-                <<<NEWHTML
-                    <div class="table4">
-    <p>According to our database, your search of: <strong>{$row[city]}</strong> has returned the following results: </p>
-    <p><strong>{$row[city]}</strong></p>
-    <p><strong>{$row[bbname]}</strong></p>
-    <p><strong>{$row[address]}</strong></p>
-    <p><strong>{$row[email]}</strong></p>
-
-
-
-
-
-
-    <p><a href="Customerinfo.html"><input type="submit" value="BOOK" /></a></p>
-
-
-
-</div>
-NEWHTML;
-            print($newhtml);
-        }
-    }
-    catch(PDOException $e)
-    {print"$e";}
-    ?>
-
-
-
-    <?php
-
-
+    foreach($st->fetchAll() as $row)
 
 
 
