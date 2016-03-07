f00ca8ebe02d181159e38ec6b1fdd1a136ee6106
Iain Scott <odyssey3007@hotmail.com>
2016-03-07 22:18:03 +0000

    go!

--- testmap.php
+++ testmap.php
@@ -1,15 +1,16 @@
+<?php
+header("Content-type: text/xml");
+echo "<?xml version='1.0' encoding='UTF-8'?>";
+echo "<note>";
+echo "<from>Jani</from>";
+echo "<to>Tove</to>";
+echo "<message>Remember me this weekend</message>";
+echo "</note>";
+?>
 <html>
 <head>
 
-    <?php
-    header("Content-type: text/xml");
-    echo "<?xml version='1.0' encoding='UTF-8'?>";
-    echo "<note>";
-    echo "<from>Jani</from>";
-    echo "<to>Tove</to>";
-    echo "<message>Remember me this weekend</message>";
-    echo "</note>";
-    ?>
+
 </head>
 
 </body>
