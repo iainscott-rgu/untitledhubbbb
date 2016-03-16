1c91435a51956b61982c51a7467c8732c7a0b092
Iain Scott <odyssey3007@hotmail.com>
2016-03-16 01:38:30 +0000

    go!

--- SearchBB.php
+++ SearchBB.php
@@ -271,21 +271,7 @@ NEWHTML;
                 </tr>
 
                </table>
-
-            <nav role="sub">
-                <ul>
-                    <li><a href="B&Bregistration.html">Contact</a></li>
-                    <li><a href="B&Bregistration.html">Register</a></li>
-                    <li><a href="OwnerSignIn.php">Member Area</a></li>
-                    <li><a href="SearchBB.php">Search</a></li>
-                </ul>
-            </nav>
-
-
-
-
-
-
+fffffffffffffffffffffffffffffffffffffffffffff
 
         </div>
     </div>
