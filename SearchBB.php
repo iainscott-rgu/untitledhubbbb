6a64c83c00932bade03d30c32139172201667676
Iain Scott <odyssey3007@hotmail.com>
2016-03-16 01:46:38 +0000

    go!

--- SearchBB.php
+++ SearchBB.php
@@ -271,7 +271,7 @@ NEWHTML;
                 </tr>
 
                </table>
-
+            <div id="footernav">
             <nav role="sub30">
                 <ul>
                     <li><a href="B&Bregistration.html">Contact</a></li>
@@ -280,6 +280,10 @@ NEWHTML;
                     <li><a href="SearchBB.php">Search</a></li>
                 </ul>
             </nav>
+                </div>
+
+
+
 
         </div>
     </div>
