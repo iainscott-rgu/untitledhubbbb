fb3c7b60e5f9ca714799e02f489b81a6086b36de
Iain Scott <odyssey3007@hotmail.com>
2016-03-16 23:56:18 +0000

    go!

--- ownerRegistrationResultPage.php
+++ ownerRegistrationResultPage.php
@@ -41,7 +41,7 @@
 <section class="container" id="featured">
     <div class="centre">
 
-        <p>You're almost there... please enter your details to complete your booking</p>
+        <p>Enter your room details...</p>
     </div>
 </section>
 
@@ -57,32 +57,47 @@
 
                 <td><label for="bbid">B&B ID: *</label></td>
                 <td><input type="text" id="bbid" class="inputform" name="bbid" value="Enter your First Name" required /></td>
-            </tr>
-            <tr>
-                <td><label for="roomname">Room Name: *</label></td>
-                <td><input type="text" id="roomname" class="inputform" name="roomname" value="Enter your First Name" required /></td>
-
-                <td><label for="roomdescription">Room Description: *</label></td>
-                <td><input type="text" id="roomdescription" class="inputform" name="roomdescription" value="Enter your First Name" required /></td>
-
-            </tr>
-            <tr>
-
-                <td><label for="price">Price: *</label></td>
-                <td><input type="text" id="price" class="inputform" name="price" value="Enter your Surname" required /></td>
 
                 <td>
                     <label for="numberofadults">Nbr. of Adults: *</label></td>
                 <td><input type="text" id="numberofadults" class="inputform" name="numberofadults" placeholder=" Enter your telephone number" size="20" maxlength="20" required /></td>
 
+
             </tr>
-            <tr><td>
+            <tr>
+                <td><label for="roomname">Room Name: *</label></td>
+                <td><input type="text" id="roomname" class="inputform" name="roomname" value="Enter your First Name" required /></td>
+
+
+
+                <td>
                     <label for="numberofchildren">Nbr. of Children: *</label></td>
                 <td><input type="text" id="numberofchildren" class="inputform" name="numberofchildren" placeholder=" Enter first line of your address" size="30" maxlength="50" required /></td>
+
+
+            </tr>
+            <tr>
+
+
+
+                <td><label for="roomdescription">Room Description: *</label></td>
+                <td><input type="text" id="roomdescription" class="inputform" name="roomdescription" value="Enter your First Name" required /></td>
 
                 <td>
                     <label for="imageurl">Image URL: *</label></td>
                 <td><input type="text" id="imageurl" class="inputform" name="imageurl" placeholder=" Enter first line of your address" size="30" maxlength="50" required /></td>
+
+
+            </tr>
+            <tr>
+
+
+
+                <td><label for="price">Price: *</label></td>
+                <td><input type="text" id="price" class="inputform" name="price" value="Enter your Surname" required /></td>
+
+
+
 
 
             </tr>
