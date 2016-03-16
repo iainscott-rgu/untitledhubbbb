39b77698c6bade1f5b69db88dd38953700c7129a
Iain Scott <odyssey3007@hotmail.com>
2016-03-16 20:03:34 +0000

    go!

--- OwnerRegistration.php
+++ OwnerRegistration.php
@@ -142,7 +142,7 @@ session_start();
                 </td>
             </tr>
 
-            <tr><td><a href="help.html" id="textlink">need help?</a></td>
+            <tr><td><a href="help.html" id="textlink"><strong>need help?</strong></a></td>
                 <td><p><input id="submit" type="submit" value="Submit" class="submit" /></p></td>
             </tr>
         </table></form>
