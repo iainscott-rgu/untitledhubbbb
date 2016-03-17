78d18a7289707be45e7fa2cea6a279e79e60de02
Iain Scott <odyssey3007@hotmail.com>
2016-03-17 22:37:17 +0000

    go!

--- searchResultsPage.php
+++ searchResultsPage.php
@@ -9,6 +9,94 @@
     <link href='https://fonts.googleapis.com/css?family=Roboto' rel='stylesheet' type='text/css'>
     <script src="https://maps.googleapis.com/maps/api/js"></script>
     <title>Search Results: theB&Bhub</title>
+
+
+
+
+
+
+
+
+
+
+    <script type="text/javascript">
+        //<![CDATA[
+
+        var tabLinks = new Array();
+        var contentDivs = new Array();
+
+        function init() {
+
+            // Grab the tab links and content divs from the page
+            var tabListItems = document.getElementById('tabs').childNodes;
+            for ( var i = 0; i < tabListItems.length; i++ ) {
+                if ( tabListItems[i].nodeName == "LI" ) {
+                    var tabLink = getFirstChildWithTagName( tabListItems[i], 'A' );
+                    var id = getHash( tabLink.getAttribute('href') );
+                    tabLinks[id] = tabLink;
+                    contentDivs[id] = document.getElementById( id );
+                }
+            }
+
+            // Assign onclick events to the tab links, and
+            // highlight the first tab
+            var i = 0;
+
+            for ( var id in tabLinks ) {
+                tabLinks[id].onclick = showTab;
+                tabLinks[id].onfocus = function() { this.blur() };
+                if ( i == 0 ) tabLinks[id].className = 'selected';
+                i++;
+            }
+
+            // Hide all content divs except the first
+            var i = 0;
+
+            for ( var id in contentDivs ) {
+                if ( i != 0 ) contentDivs[id].className = 'tabContent hide';
+                i++;
+            }
+        }
+
+        function showTab() {
+            var selectedId = getHash( this.getAttribute('href') );
+
+            // Highlight the selected tab, and dim all others.
+            // Also show the selected content div, and hide all others.
+            for ( var id in contentDivs ) {
+                if ( id == selectedId ) {
+                    tabLinks[id].className = 'selected';
+                    contentDivs[id].className = 'tabContent';
+                } else {
+                    tabLinks[id].className = '';
+                    contentDivs[id].className = 'tabContent hide';
+                }
+            }
+
+            // Stop the browser following the link
+            return false;
+        }
+
+        function getFirstChildWithTagName( element, tagName ) {
+            for ( var i = 0; i < element.childNodes.length; i++ ) {
+                if ( element.childNodes[i].nodeName == tagName ) return element.childNodes[i];
+            }
+        }
+
+        function getHash( url ) {
+            var hashPos = url.lastIndexOf ( '#' );
+            return url.substring( hashPos + 1 );
+        }
+
+        //]]>
+    </script>
+
+
+
+
+
+
+
 </head>
 <body>
 
@@ -91,11 +179,34 @@
 <table border="0" cellpadding="5">
 <tr>
 <td><strong><img src="{$row[imageurl]}" id="img3"></strong></td>
-
 <td>
 <table border="0" cellpadding="5">
 
-<tr>
+
+
+<body onload="init()">
+
+
+    <ul id="tabs">
+        <li><a href="#regions">Regional Area's</a></li>
+        <li><a href="#cities">Town's & City's</a></li>
+        <li><a href="#areas">Most Popular</a></li>
+    </ul>
+
+    <div class="tabContent" id="regions">
+
+        <div>
+            <p>Looking for a B&B in a Region of the UK but not sure where to stay? Find all B&B's by Region...</p>
+            <p>
+
+            <table border="0" class="tablejava">
+                <tr>
+
+
+
+
+
+
 <td colspan="2">B&B Name: <strong>{$row[bbname]}</strong></td>
 </tr>
 <tr>
@@ -116,8 +227,96 @@
 </tr>
 <tr>
 <td>Pets allowed: <strong>{$row[pets]}</strong></td>
-</tr>
-</h6>
+
+
+
+
+
+
+
+
+                </tr>
+
+            </table>
+
+
+
+
+
+
+        </div>
+    </div>
+
+    <div class="tabContent" id="cities">
+
+        <div>
+            <p>Looking for a B&B in a City of the UK but not sure where to stay? Find all B&B's by City...</p>
+            <p>
+
+            <table border="0" class="tablejava">
+                <tr>
+
+
+
+
+
+
+
+
+
+
+                </tr>
+
+            </table>
+
+
+        </div>
+    </div>
+
+    <div class="tabContent" id="areas">
+
+        <div>
+            <p>Looking for a B&B in a Area of the UK but not sure where to stay? Find all B&B's by Area...</p>
+            <p>
+
+            <table border="0" class="tablejava">
+                <tr>
+
+
+
+
+
+
+
+
+
+
+
+                </tr>
+
+            </table>
+
+
+        </div>
+    </div>
+
+
+<section class="spacer" id="spacer">
+
+
+</section>
+
+
+
+
+
+
+
+
+
+
+
+
 </table>
 </td>
 </tr>
