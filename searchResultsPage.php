53e93d3b85c394544a4935273fb0fc303f6d9214
Iain Scott <odyssey3007@hotmail.com>
2016-03-18 00:03:31 +0000

    go!

--- searchResultsPage.php
+++ searchResultsPage.php
@@ -176,7 +176,7 @@
                     <div class="table5">
 
 
-<table border="1" cellpadding="5">
+<table border="1" cellpadding="0">
 <tr>
 <td><strong><img src="{$row[imageurl]}" id="img3"></strong></td>
 <td valign="top">
