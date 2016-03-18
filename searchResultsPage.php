9c107bda4574e016bdeb030f38ba2f7fc833ff71
Iain Scott <odyssey3007@hotmail.com>
2016-03-18 00:29:48 +0000

    go!

--- searchResultsPage.php
+++ searchResultsPage.php
@@ -209,7 +209,7 @@ Check-out: <strong>{$row[checkout]}</strong></p>
     <div class="tabContent" id="description">
 
         <div>
-            B&B Description: <strong>{$row[bbdescription]}</strong>
+            B&B Description bbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbb: <strong>{$row[bbdescription]}</strong>
 
         </div>
     </div>
